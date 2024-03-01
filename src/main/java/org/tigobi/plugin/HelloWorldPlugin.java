23ae4d3a76536d21f0035da23ad58ed4d5d09c49
Tikhon <tihonbikbaev@gmail.com>
2024-03-01 23:20:10 +0300

    If i lose it alll

--- src/main/java/org/tigobi/plugin/HelloWorldPlugin.java
+++ src/main/java/org/tigobi/plugin/HelloWorldPlugin.java
@@ -6,22 +6,23 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
 public class HelloWorldPlugin extends JavaPlugin {
-    private  BukkitTask myTask;
+    private BukkitTask task;
 
     @Override
     public void onEnable() {
         getLogger().info("Plugin enabled");
-        var myRunnable = new BukkitRunnable() {
+        var runnable = new BukkitRunnable() {
             @Override
             public void run() {
                 Bukkit.broadcastMessage("Timer is working");
             }
         };
-         myTask = myRunnable.runTaskTimer(this, 0, 100);
+        task = runnable.runTaskTimer(this, 0, 100);
     }
+
     @Override
     public void onDisable() {
-        myTask.cancel();
+        task.cancel();
         getLogger().info("Plugin disabled");
     }
 }
