da538a81894b11e3f36067b2e061c51dd7c254a7
Tikhon <tihonbikbaev@gmail.com>
2024-03-01 22:27:39 +0300

    Всё через таску

--- src/main/java/org/tigobi/plugin/HelloWorldPlugin.java
+++ src/main/java/org/tigobi/plugin/HelloWorldPlugin.java
@@ -3,38 +3,27 @@ package org.tigobi.plugin;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 public class HelloWorldPlugin extends JavaPlugin {
     private boolean running = true;
+    private BukkitRunnable myTask;
 
     public void onEnable() {
+
         getLogger().info("Plugin enabled");
-        BukkitRunnable myTask = new BukkitRunnable() {
+        BukkitRunnable myRunnable = new BukkitRunnable() {
             @Override
             public void run() {
                 Bukkit.broadcastMessage("Таймер работает(кажется) ");
-                getLogger().info("Таймер работает");
             }
-        }.runTaskTimer(this, 0, 120);
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (!running) {
-                    this.cancel();
-                }
-                Bukkit.broadcastMessage("Таймер работает(кажется) ");
-                getLogger().info("Таймер работает");
-            }
-        }.runTaskTimer(this, 0, 120);
+        };
+        BukkitTask myTask = myRunnable.runTaskTimer(this, 20L, 100);
     }
 
-    public void stopRunning() {
-        this.running = false;
-    }
 
     public void onDisable() {
-        stopRunning();
+        myTask.cancel();
         getLogger().info("Plugin disabled");
     }
-
 }
