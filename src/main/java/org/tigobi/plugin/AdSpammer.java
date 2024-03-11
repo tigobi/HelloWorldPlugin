52345e72665edc76410f87bbcb78444152590aa9
Tikhon <tihonbikbaev@gmail.com>
2024-03-11 22:22:27 +0300

    Оригинальное сообщение

--- src/main/java/org/tigobi/plugin/AdSpammer.java
+++ src/main/java/org/tigobi/plugin/AdSpammer.java
@@ -8,11 +8,12 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class AdSpammer implements CommandExecutor {
     private final Plugin plugin;
-    private final HashMap<Integer, BukkitTask> messageTasks = new HashMap<>();
+    private final HashMap<Integer, ArrayList<BukkitTask>> messageTasks = new HashMap<>();
 
     public AdSpammer(Plugin plugin) {
         this.plugin = plugin;
@@ -23,23 +24,50 @@ public class AdSpammer implements CommandExecutor {
         int playerId = commandSender.hashCode();
         String message = argsToStringFrom2Element(args);
         if (args[0].equalsIgnoreCase("start")) {
-            if (messageTasks.get(playerId) == null || messageTasks.get(playerId).isCancelled()) {
-                commandSender.sendMessage("adSpammer start");
-                messageTasks.put(playerId, startTimerTask(message));
-            } else {
-                commandSender.sendMessage("Spam is already running!");
+            if (messageTasks.get(playerId) == null) {
+                messageTasks.put(playerId, new ArrayList<BukkitTask>());
             }
+            commandSender.sendMessage("adSpammer start");
+            messageTasks.get(playerId).add(startTimerTask(message));
             return true;
         }
         if (args[0].equalsIgnoreCase("stopAll")) {
-            stopAll(messageTasks);
+            stopAll();
             commandSender.sendMessage("adSpammer stopAll");
         }
+        if (args[0].equalsIgnoreCase("list")) {
+            if (messageTasks.get(playerId) == null || messageTasks.get(playerId).size() == 0) {
+                commandSender.sendMessage("You don't have tasks running");
+            } else if (messageTasks.get(playerId).size() == 1) {
+                commandSender.sendMessage("You have 1 task.");
+            } else {
+                commandSender.sendMessage("You have " + messageTasks.get(playerId).size() + " tasks.");
+            }
+            return true;
+        }
         if (args[0].equalsIgnoreCase("stop")) {
+
             if (messageTasks.get(playerId) != null) {
+                if (args[1].equalsIgnoreCase("all")) {
+                    stopAllArrayTasks(messageTasks.get(playerId));
+                    commandSender.sendMessage("Stopped all your tasks🥰");
+                    return true;
+                }
+                int stopTaskNumber = -1;
+                String invalidTaskNumber = "Invalid task number should be \"all\" from 1 to " + messageTasks.get(playerId).size();
+                try {
+                    stopTaskNumber = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    commandSender.sendMessage(invalidTaskNumber);
+                    return false;
+                }
+                if (stopTaskNumber > messageTasks.get(playerId).size() || stopTaskNumber < 1) {
+                    commandSender.sendMessage(invalidTaskNumber);
+                    return false;
+                }
                 commandSender.sendMessage("adSpammer stop");
-                messageTasks.get(playerId).cancel();
-                messageTasks.remove(playerId);
+                messageTasks.get(playerId).get(stopTaskNumber - 1).cancel();
+                messageTasks.get(playerId).remove(stopTaskNumber - 1);
             } else {
                 commandSender.sendMessage("You don't have tasks running");
             }
@@ -48,11 +76,19 @@ public class AdSpammer implements CommandExecutor {
         return false;
     }
 
-    private void stopAll(HashMap<Integer, BukkitTask> map) {
-        var iterator = map.entrySet().iterator();
+    private void stopAllArrayTasks(ArrayList<BukkitTask> tasks) {
+        var arrayIterator = tasks.iterator();
+        while (arrayIterator.hasNext()) {
+            var arrayEntry = arrayIterator.next();
+            arrayEntry.cancel();
+            arrayIterator.remove();
+        }
+    }
+
+    private void stopAll() {
+        var iterator = messageTasks.entrySet().iterator();
         while (iterator.hasNext()) {
-            var entry = iterator.next();
-            entry.getValue().cancel();
+            stopAllArrayTasks(iterator.next().getValue());
             iterator.remove();
         }
     }
